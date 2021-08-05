9ac1d228c819a298b7df35ec30a48027751722f2
lowkey <saltyndea@gmail.com>
2021-08-05 14:22:55 -0600

    Added leaflet with mapbox map

--- index.js
+++ index.js
@@ -2,13 +2,34 @@ $searchBtn = $('.search-button');
 $weather = $('.weather');
 $search = $('.search');
 $box = $('.box');
+let lat;
+let lng;
+
+const renderMap = function() {
+  const myMap = L.map('weather__map').setView([lat, lng], 13);
+  
+
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 12,
+    id: 'mapbox/outdoors-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibG93a2V5Y29kZSIsImEiOiJja29uMnRkOTcwNmx6MnhwaHZtMzloeWs2In0.t2Kl5vjXD17B_caNpFJWCg'
+  }).addTo(myMap);
+
+
+  return myMap;
+}
 
 const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + 1) + min;
 
 const getWeatherBackground = async function(weatherData) {
-  const $heroImg = $('.hero-container__img');
+  $hereoImgContainer = $('.hero-container');
+  $heroImg = $('.hero-container__img');
 
   try {
+    $hereoImgContainer.addClass('marginalize');
     const superSecretKey = 'ZPnBuAWa6RtJh9NzLdZjhURL9jwQ7Li7sIXqpympOqs';
     console.log(weatherData.weather[0].description);
     
@@ -39,6 +60,9 @@ const renderCity = function(data) {
   $weather.append( `
     <div class="weather__location">
       <div class="weather__location--name">${data.name}, ${data.sys.country}</div>
+      <div class="weather__location--close">
+        <img class="close" src="./img/close.svg" alt="Close searched city">
+      </div>
     </div>
 
     <div class="weather__temp">
@@ -53,10 +77,11 @@ const renderCity = function(data) {
       <div class="weather__forecast--copy">${data.weather[0].main}</div>
     </div>
 
-    <div class="weather__map">
+    <div class="weather__map" id="weather__map">
       MAP 
     </div>
   `);
+  renderMap();
 }
 
 const getWeather = async function() {
@@ -64,7 +89,7 @@ const getWeather = async function() {
     const $city = $('.search-input').val();
 
     if(!$city) {
-      alert('Please select a city!')
+      alert('Please select a city!');
       return;
     }
 
@@ -75,6 +100,11 @@ const getWeather = async function() {
       dataType: "json"
     });
 
+
+    lat = data.coord.lat;
+    lng = data.coord.lon;
+
+    console.log(lat, lng);
     
     renderCity(data);
      await getWeatherBackground(data); 
@@ -89,3 +119,6 @@ const getWeather = async function() {
 
 
 $searchBtn.on('click', getWeather);
+$(document).on('keypress', function(e) {
+  if(e.key === 'Enter') getWeather();
+})
