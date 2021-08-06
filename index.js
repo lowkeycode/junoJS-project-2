f99fe9864abb9b16a62a82ffdec64fb3ba1bff03
lowkey <saltyndea@gmail.com>
2021-08-06 12:00:03 -0600

    Refactored for namespacing

--- index.js
+++ index.js
@@ -2,29 +2,108 @@ $searchBtn = $(".search-button");
 $weather = $(".weather");
 $search = $(".search");
 $box = $(".box");
-$heroImgContainer = $(".hero-container");
 $heroImg = $(".hero-container__img");
 $title = $('.title');
-$logo = $('.logo');
-let lat;
-let lng;
+$logo = $(".logo");
 
-const handleClose = function (e) {
-  $heroImgContainer = $(".hero-container");
-  $heroImgContainer.removeClass('marginalize');
-  $heroImg = $(".hero-container__img");
-  $heroImg.attr("src", './img/aas-hero.jpg');
-  $logo.removeClass('skooch');
-  $title.removeClass('skooch');
-  $weather.empty();
-  $weather.addClass("hide");
-  $box.removeClass("box-display");
-  $search.removeClass("hide");
-  lat = lng = undefined;
+const app = {};
+
+app.getWeather = async function () {
+  try {
+    
+    const $city = $(".search-input").val();
+
+    if (!$city) {
+      alert("Please select a city!");
+      return;
+    }
+
+    const superSecretKey = "e9108d4762c5ac46b2c2d8c47fb0b923";
+    const data = await $.ajax({
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${$city}&appid=${superSecretKey}`,
+      method: "GET",
+      dataType: "json",
+    });
+
+    app.lat = data.coord.lat;
+    app.lng = data.coord.lon;
+
+    app.renderCity(data);
+    await app.getWeatherBackground(data);
+
+    $(".search-input").val("");
+    $(document).off('keypress');
+  } catch (err) {
+    console.log(err);
+    alert("City not found. Please try again!");
+    $(".search-input").val("");
+  }
 };
 
-const renderMap = function () {
-  const myMap = L.map("weather__map").setView([lat, lng], 13);
+app.renderCity = function (data) {
+  const icon = data.weather[0].icon;
+  const kelvinToCelsius = Math.round(data.main.temp - 273.15);
+
+  $search.addClass("hide");
+  $box.addClass("box-display");
+  $weather.removeClass("hide");
+
+  $weather.append(`
+    <div class="weather__location">
+      <div class="weather__location--name">${data.name}, ${data.sys.country}</div>
+      <div class="weather__location--close">
+        <img class="close" src="./img/close.svg" alt="Close searched city">
+      </div>
+    </div>
+
+    <div class="weather__temp">
+      <div class="weather__temp--copy">Temperature</div>
+      <div class="weather__temp--deg">${kelvinToCelsius}&deg;C</div>
+    </div>
+
+    <div class="weather__forecast">
+      <figure class="weather__forecast--icon">
+        <img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="Weather symbol">
+      </figure>
+      <div class="weather__forecast--copy">${data.weather[0].main}</div>
+    </div>
+
+    <div class="weather__map" id="weather__map">
+      MAP 
+    </div>
+  `);
+  app.renderMap();
+
+  $closeBtn = $(".weather__location--close");
+  $closeBtn.on("click", app.handleClose);
+};
+
+app.getWeatherBackground = async function (weatherData) {
+  try {
+
+    const superSecretKey = "ZPnBuAWa6RtJh9NzLdZjhURL9jwQ7Li7sIXqpympOqs";
+
+    const data = await $.ajax({
+      url: `https://api.unsplash.com/search/photos?client_id=${superSecretKey}&page=1&query=${weatherData.name}`,
+      method: "GET",
+      dataType: "json",
+    });
+    const randomIndex = app.randomInt(0, 9);
+    const weatherBackgroundUrl = data.results[randomIndex].urls.regular;
+
+    $title.addClass("hide");
+    $logo.addClass("hide");
+
+    $heroImg.attr("src", weatherBackgroundUrl);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.randomInt = (min, max) => Math.floor(Math.random() * (max - min) + 1) + min;
+
+app.renderMap = function () {
+  const myMap = L.map("weather__map").setView([app.lat, app.lng], 13);
 
   L.tileLayer(
     "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
@@ -44,103 +123,44 @@ const renderMap = function () {
   return myMap;
 };
 
-const randomInt = (min, max) =>
-  Math.floor(Math.random() * (max - min) + 1) + min;
-
-const getWeatherBackground = async function (weatherData) {
-
-  try {
-    $heroImgContainer.addClass("marginalize");
-    $logo.addClass('skooch');
-    $title.addClass('skooch');
-    const superSecretKey = "ZPnBuAWa6RtJh9NzLdZjhURL9jwQ7Li7sIXqpympOqs";
-    console.log(weatherData.weather[0].description);
-
-    const data = await $.ajax({
-      url: `https://api.unsplash.com/search/photos?client_id=${superSecretKey}&page=1&query=${weatherData.weather[0].main}%20weather`,
-      method: "GET",
-      dataType: "json",
-    });
-    const randomIndex = randomInt(0, 9);
-    const weatherBackgroundUrl = data.results[randomIndex].urls.full;
-
-    $heroImg.attr("src", weatherBackgroundUrl);
-  } catch (error) {
-    console.log(error);
-  }
+app.handleClose = function (e) {
+  $heroImg.attr("src", './img/aas-hero.jpg');
+  $weather.empty();
+  $weather.addClass("hide");
+  $box.removeClass("box-display");
+  $title.removeClass("hide");
+  $logo.removeClass("hide");
+  $search.removeClass("hide");
+  app.lat = app.lng = undefined;
+  $(document).on("keypress", function (e) {
+    if (e.key === "Enter") app.getWeather();
+  });
 };
 
-const renderCity = function (data) {
-  const icon = data.weather[0].icon;
-  const celsius = Math.round(data.main.temp - 273.15);
+app.init = function() {
+  $searchBtn.on("click", app.getWeather);
+  $(document).on("keypress", function (e) {
+    if (e.key === "Enter") app.getWeather();
+  });
 
-  $search.addClass("hide");
-  $box.addClass("box-display");
-  $weather.removeClass("hide");
+}
 
-  $weather.append(`
-    <div class="weather__location">
-      <div class="weather__location--name">${data.name}, ${data.sys.country}</div>
-      <div class="weather__location--close">
-        <img class="close" src="./img/close.svg" alt="Close searched city">
-      </div>
-    </div>
+$('document').ready(function() {
+  app.init();
 
-    <div class="weather__temp">
-      <div class="weather__temp--copy">Temperature</div>
-      <div class="weather__temp--deg">${celsius}&deg;C</div>
-    </div>
-
-    <div class="weather__forecast">
-      <figure class="weather__forecast--icon">
-        <img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="Weather symbol">
-      </figure>
-      <div class="weather__forecast--copy">${data.weather[0].main}</div>
-    </div>
-
-    <div class="weather__map" id="weather__map">
-      MAP 
-    </div>
-  `);
-  renderMap();
-
-  $closeBtn = $(".weather__location--close");
-  $closeBtn.on("click", handleClose);
-};
-
-const getWeather = async function () {
-  try {
-    const $city = $(".search-input").val();
-
-    if (!$city) {
-      alert("Please select a city!");
-      return;
-    }
-
-    const superSecretKey = "e9108d4762c5ac46b2c2d8c47fb0b923";
-    const data = await $.ajax({
-      url: `https://api.openweathermap.org/data/2.5/weather?q=${$city}&appid=${superSecretKey}`,
-      method: "GET",
-      dataType: "json",
-    });
-
-    lat = data.coord.lat;
-    lng = data.coord.lon;
-    console.log(lat, lng);
-
-    renderCity(data);
-    await getWeatherBackground(data);
-
-    $(".search-input").val("");
-    console.log(data);
-  } catch (err) {
-    console.log(err);
-    alert("City not found. Please try again!");
-    $(".search-input").val("");
-  }
-};
-
-$searchBtn.on("click", getWeather);
-$(document).on("keypress", function (e) {
-  if (e.key === "Enter") getWeather();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
